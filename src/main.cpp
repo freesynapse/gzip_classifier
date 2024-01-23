92af0f4b5b492c6c2bdd278289fb135a7bcf0d75
freesynapse <mans.edstrom@gmail.com>
2024-01-23 11:08:57 +0100

    cleaner

--- src/main.cpp
+++ src/main.cpp
@@ -27,9 +27,12 @@ using namespace Syn;
 
 
 // function prototypes
+//
 size_t zip(const char *_input_text, size_t len);
 
+
 // data structures
+//
 typedef struct sample_t
 {
     int class_ = -1;
@@ -64,11 +67,19 @@ typedef struct thread_info_t
 {
     size_t sample_offset;
     size_t chunk_size;
-    sample_t *input_sample;
+    sample_t *input_sample_ptr;
     sample_t *samples_ptr;  // read and write, threaded
     ncd_t *ncds_ptr;
     
 } thread_info_t;
+
+
+// globals
+//
+size_t      g_sample_count = 0;     // number of samples
+sample_t   *g_samples      = NULL;  // samples to learn from
+ncd_t      *g_ncds         = NULL;  // buffer of noramlized compressed distances between 
+                                    // input and samples
 
 //---------------------------------------------------------------------------------------
 // function for sorting ncd_t using qsort
@@ -94,24 +105,7 @@ size_t zip(const char *_input_text, size_t _len)
 }
 
 //---------------------------------------------------------------------------------------
-void parse_training_data(const char *_filename, std::vector<sample_t> &_training_samples)
-{
-    std::ifstream fin(_filename, std::ios::in | std::ios::app);
-    std::string line;
-    std::getline(fin, line);    // skip header
-    size_t i = 0;
-    while (std::getline(fin, line))
-    {
-        _training_samples.push_back(sample_t(line));
-        i++;
-    }
-
-    fin.close();
-    
-}
-
-//---------------------------------------------------------------------------------------
-void parse_training_data2(const char *_filename, sample_t *_samples)
+void parse_training_data(const char *_filename, sample_t *_samples)
 {
     std::ifstream fin(_filename, std::ios::in | std::ios::app);
     std::string line;
@@ -157,6 +151,25 @@ void *compress_threaded(void *_thread_info)
 }
 
 //---------------------------------------------------------------------------------------
+void compress_samples(thread_info_t *_ti)
+{
+    int ncores = get_nprocs();
+    pthread_t threads[ncores];
+
+    LOG_INFO("Compressing samples...\n");
+    {
+        // Timer t("[INFO] threaded sample compression", true);
+        for (int i = 0; i < ncores; i++)
+            pthread_create(&threads[i], NULL, compress_threaded, &_ti[i]);
+
+        for (int i = 0; i < ncores; i++)
+            pthread_join(threads[i], NULL);
+
+    }
+
+}
+
+//---------------------------------------------------------------------------------------
 float ncd(sample_t *_x, sample_t *_y)
 {
     // (from https://arxiv.org/pdf/2212.09410.pdf)
@@ -192,72 +205,47 @@ void *ncd_threaded(void *_thread_info)
     // calculate ncd
     for (size_t i = i0; i < i1; i++)
     {
-        ti->ncds_ptr[i].distance = ncd(ti->input_sample, &ti->samples_ptr[i]);
+        ti->ncds_ptr[i].distance = ncd(ti->input_sample_ptr, &ti->samples_ptr[i]);
         ti->ncds_ptr[i].class_ = ti->samples_ptr[i].class_;
     }
     
     return NULL;
-        
+
 }
 
 //---------------------------------------------------------------------------------------
-void classify_sample(sample_t *_input_sample, sample_t *_samples, size_t _n, int _k)
+void classify_sample(sample_t *_input_sample, 
+                     int _k, 
+                     thread_info_t *_ti)
 {
     //
     int ncores = get_nprocs();
     pthread_t threads[ncores];
-    thread_info_t tis[ncores];
 
     //
-    ncd_t *ncds = (ncd_t *)malloc(sizeof(ncd_t) * _n);
+    assert(g_ncds != NULL);
+    memset(g_ncds, 0, sizeof(ncd_t) * g_sample_count);
 
-    // for splitting work loads between the threads
-    int chunk_size = _n / (size_t)ncores;
-    int remain = _n - (ncores * chunk_size);
-    
-    LOG_INFO("(2) Compressing samples...\n");
+    LOG_INFO("Calulating NCDs...\n");
     {
-        Timer t("[INFO] threaded compression", true);
+        // Timer t("[INFO] threaded NCD calculation", true);
         for (int i = 0; i < ncores; i++)
-        {
-            int end = 0;
-            if (i == ncores - 1) end = remain;
-            
-            tis[i].sample_offset    = i * chunk_size;
-            tis[i].chunk_size       = chunk_size + end;
-            tis[i].samples_ptr      = _samples;
-            tis[i].input_sample     = _input_sample;
-            tis[i].ncds_ptr         = ncds;
-
-            pthread_create(&threads[i], NULL, compress_threaded, &tis[i]);
-
-        }
-
-        for (int i = 0; i < ncores; i++)
-            pthread_join(threads[i], NULL);
-
-    }
-
-    LOG_INFO("(3) Calulating NCDs...\n");
-    {
-        Timer t("[INFO] threaded NCD calculation", true);
-        for (int i = 0; i < ncores; i++)
-            pthread_create(&threads[i], NULL, ncd_threaded, &tis[i]);
+            pthread_create(&threads[i], NULL, ncd_threaded, &_ti[i]);
         
         for (int i = 0; i < ncores; i++)
             pthread_join(threads[i], NULL);
 
     }
 
-    // 4. Classify using K-nearest neighbours
+    // Classify using K-nearest neighbours
     //
 
     // sort ndcs by shortest distance and make the knn vote
-    qsort(ncds, _n, sizeof(ncd_t), cmp_ncd_distance);
+    qsort(g_ncds, g_sample_count, sizeof(ncd_t), cmp_ncd_distance);
 
     int ks[CLASS_COUNT] = { 0 };
     for (size_t i = 0; i < _k; i++)
-        ks[ncds[i].class_]++;
+        ks[g_ncds[i].class_]++;
 
     // find max class
     int pred_class = -1;
@@ -267,16 +255,36 @@ void classify_sample(sample_t *_input_sample, sample_t *_samples, size_t _n, int
         if (ks[i] > max)
         {
             max = ks[i];
-            pred_class = i+1;
+            pred_class = i;
         }
     }
 
-    LOG_INFO("(4) Classified sample as class %d.\n", pred_class);
+    static std::string classes[4] =
+    {
+        "World",
+        "Sports",
+        "Business",
+        "Sci/Tech",
+    };
+
+    LOG_INFO("Classified sample as class '%s' (%d).\n", classes[pred_class].c_str(), pred_class+1);
     for (int i = 0; i < CLASS_COUNT; i++)
         LOG_INFO("    class %d: %d\n", i+1, ks[i]);
 
-    free(ncds);
+}
 
+//---------------------------------------------------------------------------------------
+void init(size_t _n)
+{
+    g_samples = (sample_t *)malloc(sizeof(sample_t) * _n);
+    g_ncds = (ncd_t *)malloc(sizeof(ncd_t) * _n);
+}
+
+//---------------------------------------------------------------------------------------
+void shutdown()
+{
+    free(g_samples);
+    free(g_ncds);
 }
 
 //---------------------------------------------------------------------------------------
@@ -286,21 +294,67 @@ int main(int argc, char *argv[])
 
     const char *train_file = "../data/train.csv";
 
-    // allocate memory for training data
-    size_t n = line_count(train_file);
-    sample_t *samples = (sample_t *)malloc(sizeof(sample_t) * n);
+    // allocate memory for samples
+    g_sample_count = line_count(train_file);
+    init(g_sample_count);
 
-    LOG_INFO("(1) Parsing training data...\n");
-    parse_training_data2(train_file, samples);
+    LOG_INFO("(1) Parsing samples...\n");
+    parse_training_data(train_file, g_samples);
 
-    // input sample -- class 3
+    // example input sample -- class 3
     const char *test_input = "Oil and Economy Cloud Stocks' Outlook (Reuters),Reuters - Soaring crude prices plus worries\about the economy and the outlook for earnings are expected to\hang over the stock market next week during the depth of the\summer doldrums.";
-    sample_t input_sample(test_input, true);
+    sample_t input_sample_ex(test_input, true);
+
+    // setup for threaded compression of samples
+    int ncores = get_nprocs();
+    int chunk_size = g_sample_count / (size_t)ncores;
+    int remain = g_sample_count - (ncores * chunk_size);
+    
+    thread_info_t tis[ncores];
+    
+    for (int i = 0; i < ncores; i++)
+    {
+        int end = 0;
+        if (i == ncores - 1) end = remain;
+        
+        tis[i].sample_offset    = i * chunk_size;
+        tis[i].chunk_size       = chunk_size + end;
+        tis[i].samples_ptr      = g_samples;
+        tis[i].input_sample_ptr = &input_sample_ex;
+        tis[i].ncds_ptr         = g_ncds;
+
+    }
+
+    // compress all samples threaded
+    compress_samples(tis);
+
+    // loop
+    std::string input;
+    bool do_continue = true;
+    while (do_continue)
+    {
+        std::cout << "input> ";
+        std::getline(std::cin, input);
+        if (input == "exit")
+            do_continue = false;
+        else if (input == "help")
+        { 
+            LOG_INFO("Please provide sample text or type 'exit'.\n");
+        }
+        else
+        {
+            sample_t input_sample(input, true);
+            for (int i = 0; i < ncores; i++)
+                tis[i].input_sample_ptr = &input_sample;
+            //
+            classify_sample(&input_sample, 500, tis);
+
+        }
+    }
+
 
     //
-    classify_sample(&input_sample, samples, n, 500);
-
-    free(samples);
+    shutdown();
 
     //
     return 0;
--- src/utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-
-//
-#define LOG_INFO(...) { printf("[INFO] " __VA_ARGS__); }
-#define LOG_ERROR(...) { printf("[ERROR] " __VA_ARGS__); }
-
